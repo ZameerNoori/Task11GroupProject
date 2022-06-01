c90a6ca0b35ed7f2215c7f4de083c5d6d9134d5d
ZameerNoori <zameer.noori3@gmail.com>
2022-06-01 00:19:45 -0400

    MyVeryFirstTask

---
+++ src/task11/MyTask11.java
@@ -0,0 +1,19 @@
+package task11;
+
+public class MyTask11 {
+
+	public static void main(String[] args) {
+		
+		/*11. Write a program to print out duplicate elements from
+        an Array of Strings? 
+		 */ 
+     String[] cars={"GT500", "Demon", "ZL1", "Demon", "GT500"};   
+       
+     for(int b=0; b<cars.length; b++) {  
+         for(int c=b+1; c<cars.length; c++) {  
+             if(cars[b] == cars[c])  
+                 System.out.print(cars[c]+" ");  
+         }  
+      }
+   }
+}
